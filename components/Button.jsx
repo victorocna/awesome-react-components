bac8602696c0b98291da88d3fd4a9e9ec25ab8fc
victorocna <victorocnarescu@gmail.com>
2021-03-08 21:24:08 +0200

    update buttons

--- components/Button.jsx
+++ components/Button.jsx
@@ -1,21 +1,7 @@
 import React from 'react';
 
-const Button = ({ className, variant, children, ...props }) => {
-  const classes = ['border-2 py-1 px-4 rounded shadow text-sm'];
-
-  switch (variant) {
-    case 'danger':
-      classes.push('border-red-700 text-red-700 hover:bg-red-700 hover:text-white');
-      break;
-    case 'outline':
-      classes.push('border-indigo-700 hover:bg-indigo-800 hover:text-white');
-      break;
-    default:
-      classes.push('border-indigo-700 bg-indigo-700 hover:bg-indigo-800 text-white font-semibold');
-      break;
-  }
-
-  // add component specific classes
+const Button = ({ variant = 'primary', className, children, ...props }) => {
+  const classes = ['form-button', `form-button-${variant}`];
   if (className) {
     classes.push(className);
   }
